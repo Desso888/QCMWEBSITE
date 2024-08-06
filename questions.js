4858d5728c46c76c2402a582fc11b749c2791251
kevin.yessoufou <kevin.yessoufou@epitech.eu>
2024-08-06 10:16:40 +0100

    QCMWEBSITE

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions =[
+    {
+        numb:1,
+        question:"Who is the most powerful in One Punch Man?",
+        answer:"C/ Saitama",
+        options:[
+            "A/ Genos",
+            "B/ Cosmic Garou",
+            "C/ Saitama",
+            "D/ Tatsumaki",
+        ]
+    },
+    {
+        numb:2,
+        question:"Who is Akira Toriyama?",
+        answer:"A/ The father of DBZ",
+        options:[
+            "A/ The father of DBZ",
+            "B/ The father of Naruto",
+            "C/ A random person",
+            "D/ The father of JJK",
+        ]
+    },
+    {
+        numb:3,
+        question:"In which manga do we hear 'Bankai'?",
+        answer:"B/ Bleach",
+        options:[
+            "A/ Naruto",
+            "B/ Bleach",
+            "C/ Dragon Ball Z",
+            "D/ Jujutsu Kaisen",
+        ]
+    },
+    {
+        numb:4,
+        question:"Who is Natsu?",
+        answer:"D/ A wizard",
+        options:[
+            "A/ A knight",
+            "B/ A clerk",
+            "C/ A kryptonian",
+            "D/ A wizard",
+        ]
+    },
+    {
+        numb:5,
+        question:"What is 'jiki jiki no mi'?",
+        answer:"A/ A devil fruit",
+        options:[
+            "A/ A devil fruit",
+            "B/ A magic sword",
+            "C/ A shield",
+            "D/ A town",
+        ]
+    },
+];
